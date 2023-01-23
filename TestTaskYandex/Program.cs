4d1604cdff7a91cadb99402b9829b14fa6a9d049
79264 <79264@DESKTOP-65729PJ>
2023-01-23 14:55:43 +0530

    Добавьте файлы проекта.

---
+++ TestTaskYandex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Example
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+
+            if (char.IsDigit(s[s.Length - 1]) || s.Contains(" "))
+            {
+                Console.WriteLine("Строка s содержит пробелы или цифры");
+            }
+            else Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
